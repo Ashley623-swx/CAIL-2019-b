ffb958daaea9ae9125bf451c06a32f1308e876c3
Ashley623-swx <550606360@qq.com>
2025-03-22 11:14:27 +0800

    Update config.py

--- config.py
+++ config.py
@@ -12,10 +12,10 @@ class Config():
     max_length = 512
     batch_size = 7
     epoch = 50
-    learning_rate = 0.5e-5
+    learning_rate = 1e-5
     weight_decay = 5e-3
     schedule = 'CosineAnnealingLR'
 
     # RCNN 相关配置
-    dropout = 0.2  # dropout 概率
+    dropout = 0.2  
     embedding_dim = 256
